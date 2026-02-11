a26e9d49ade062a27257a2195f017d0af334f4da
ArifDeveloper <achadzainul67@gmail.com>
2026-02-11 05:14:36 +0000

    Update footer component layout and content for improved structure and clarity

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <footer className="w-full">
-      <div className="max-w-7xl mx-auto px-4 py-8 text-center font-bold">Footer Here</div>
+      <div className="max-w-7xl mx-auto px-4 py-8 text-center font-bold">
+        <div className="grid grid-cols-1
+        md:grid-cols-3 flex gap-4">
+          <div>Tes</div>
+          <div>Tes2</div>
+          <div>Tes3</div>
+        </div>
+        <span className="w-full text-center">
+          &copy;2026 Syafa Workshop, All Right Reserve.
+        </span>
+      </div>
     </footer>
   );
 }
